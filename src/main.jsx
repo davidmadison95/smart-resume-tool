16d79050c12a1d0500a3867a2ae706e83d94abec
David Madison <davidmadison95@yahoo.com>
2025-11-07 13:22:03 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.css';
 
+// Using HashRouter ensures your app works on GitHub Pages
+// (BrowserRouter can break without server-side routing)
+import { HashRouter } from 'react-router-dom';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
